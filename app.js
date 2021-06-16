9e9f8a3750360202dbf99d467346ef832d273e19
ArtilleryTrigger <artillerytrigger@gmail.com>
2021-06-16 21:27:02 +0300

    init

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-console.time();
 console.log('Hello from app.js');
 console.info('some infor');
 console.warn('some warning');
 console.error('error !!!!');
+
+console.log('---------');
+
+var myName = 'Matthew';
+console.log(myName);
+myNema = 'John';
+console.log(myName);
+
+var announce = 'Welcome to our school!';
+console.log(announce);
+announce = 'Welcome to Full-stack';
+console.log(announce);
+
+var num = 4;
+console.log(num * num)
